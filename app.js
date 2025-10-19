d771abe8ec572d358579a19467b1b6c5ee4a872d
mshahzaib-web <otherworks45@gmail.com>
2025-10-19 00:29:11 +0000

    add codespace

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ main()
   })
   .catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/ecommercelocal");
+  await mongoose.connect("mongodb://localhost:27017/ecommerce");
 }
 
 const product = require("./routes/product.js");
